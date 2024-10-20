9da6ddd88340f383701d50c067c4631137c6494a
WildTooth <champenV@outlook.dk>
2024-10-20 23:14:31 +0200

    Tilføjet MessageService.java

---
+++ api/src/main/java/dk/fvtrademarket/fvplus/api/service/message/MessageService.java
@@ -0,0 +1,64 @@
+package dk.fvtrademarket.fvplus.api.service.message;
+
+import dk.fvtrademarket.fvplus.api.misc.EventMessage;
+import dk.fvtrademarket.fvplus.api.service.Service;
+import net.labymod.api.util.Pair;
+import java.util.Map;
+
+public interface MessageService extends Service {
+
+  /**
+   * Tilføjer en besked
+   * <p>
+   * Simple beskeder uden egentlige variable informationer
+   *
+   * @param messageStr  Beskedens tekst
+   * @param eventMessage Beskedtypen
+   */
+  void addMessage(String messageStr, EventMessage eventMessage);
+
+  /**
+   * Tilføjer en besked med advancerede parametre
+   * <p>
+   * En avanceret besked ses som en besked i 2 dele, hvor den første del er beskedens begyndelse og den anden del er beskedens slutning.
+   * Ideen er at man på en nem måde kan få variable informationer ud af en besked, fordi vi kender den generelle beskedstruktur.
+   * <p>
+   * Eksempel:
+   * <br> Første del: "Du har nu"
+   * <br> Anden del: "point"
+   * <br> Besked: "Du har nu 10 point"
+   * <br>
+   * Her kan vi nemt udtrække tallet 10, fordi vi kender beskedens struktur.
+   *
+   * @param messagePair  Beskedens begyndelse og slutning
+   * @param eventMessage Beskedtypen
+   */
+  void addAdvancedMessage(Pair<String, String> messagePair, EventMessage eventMessage);
+
+  /**
+   * Fjerner en besked
+   *
+   * @param eventMessage Beskedtypen
+   */
+  void removeMessage(EventMessage eventMessage);
+
+  /**
+   * Returnerer alle almindelige beskeder
+   *
+   * @return Beskederne
+   */
+  Map<String, EventMessage> getMessages();
+
+  /**
+   * Returnerer alle avancerede beskeder
+   *
+   * @return Beskederne
+   */
+  Map<Pair<String, String>, EventMessage> getAdvancedMessages();
+
+  @Override
+  void initialize();
+
+  @Override
+  void shutdown();
+}
