f0885a149f0e83298c04faec5b8f5c9596924739
WildTooth <champenv@outlook.dk>
2024-10-21 22:45:13 +0200

    Tilføjet en måde at hente specifikke typer i ActivatableService

--- api/src/main/java/dk/fvtrademarket/fvplus/api/service/activatable/ActivatableService.java
+++ api/src/main/java/dk/fvtrademarket/fvplus/api/service/activatable/ActivatableService.java
@@ -2,15 +2,32 @@ package dk.fvtrademarket.fvplus.api.service.activatable;
 
 import dk.fvtrademarket.fvplus.api.activatable.Activatable;
 import dk.fvtrademarket.fvplus.api.service.Service;
+import net.labymod.api.reference.annotation.Referenceable;
 import java.util.Collection;
 
-public interface ActivatableService<T extends Activatable> extends Service {
+/**
+ * Et service til at håndtere aktiverbare objekter
+ *
+ * @author Champen_V1ldtand
+ * @since 2.0.0
+ */
+@Referenceable
+public interface ActivatableService extends Service {
 
-  void registerActivatable(T activatable);
+  void registerActivatable(Activatable activatable);
 
-  void unregisterActivatable(T activatable);
+  void unregisterActivatable(Activatable activatable);
 
-  Collection<T> getAllActivatables();
+  Collection<Activatable> getAllActivatables();
+
+  /**
+   * Hent alle aktiverbare objekter af en bestemt type
+   *
+   * @param activatableClass klassen for de aktiverbare objekter
+   * @return alle aktiverbare objekter af den givne type
+   * @param <A> typen af aktiverbare objekter
+   */
+  <A extends Activatable> Collection<A> getActivatables(Class<A> activatableClass);
 
   @Override
   void initialize();
