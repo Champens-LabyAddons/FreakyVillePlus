6ae09c85b728141fe01b74cd585f6eee7ccfb30a
WildTooth <champenV@outlook.dk>
2024-10-22 10:54:44 +0200

    Tilføjet lidt timergeneratorer

--- core/src/main/java/dk/fvtrademarket/fvplus/core/widgets/factory/TimerHudWidgetPatterns.java
+++ core/src/main/java/dk/fvtrademarket/fvplus/core/widgets/factory/TimerHudWidgetPatterns.java
@@ -2,6 +2,7 @@ package dk.fvtrademarket.fvplus.core.widgets.factory;
 
 import dk.fvtrademarket.fvplus.api.activatable.Activatable;
 import dk.fvtrademarket.fvplus.api.activatable.guardvault.GuardVault;
+import dk.fvtrademarket.fvplus.api.activatable.misc.GangArea;
 import dk.fvtrademarket.fvplus.core.widgets.TimerHudWidget;
 import net.labymod.api.client.component.TextComponent;
 import net.labymod.api.client.component.serializer.plain.PlainTextComponentSerializer;
@@ -9,13 +10,35 @@ import net.labymod.api.client.gui.icon.Icon;
 import net.labymod.api.client.resources.ResourceLocation;
 
 public class TimerHudWidgetPatterns {
+
   public TimerHudWidget createTimerHudWidget(GuardVault guardVault) {
-    return new TimerHudWidget("guard_vault_timer_" + guardVault.getPrisonSector().name(),
+    TextComponent component = (TextComponent) guardVault.toComponent();
+    return new TimerHudWidget("guard_vault_timer_" +
+        correctFormat(componentToString(component)),
         componentToString((TextComponent) guardVault.toComponent()), getIcon(guardVault), 0L);
+  }
+
+  public TimerHudWidget createTimerHudWidget(GangArea gangArea) {
+    TextComponent component = (TextComponent) gangArea.toComponent();
+    return new TimerHudWidget("gang_area_timer_" +
+        correctFormat(componentToString(component)),
+        componentToString((TextComponent) gangArea.toComponent()), getIcon(gangArea), 0L);
+  }
+
+  public TimerHudWidget createTimerHudWidget(Activatable activatable) {
+    TextComponent component = (TextComponent) activatable.toComponent();
+    return new TimerHudWidget("activatable_misc_timer_" +
+        correctFormat(componentToString(component)),
+        componentToString(component),
+        getIcon(activatable), 0L);
   }
 
   private String componentToString(TextComponent component) {
     return PlainTextComponentSerializer.plainText().serialize(component);
+  }
+
+  private String correctFormat(String str) {
+    return str.replace(" ", "_").toLowerCase();
   }
 
   private Icon getIcon(Activatable activatable) {
