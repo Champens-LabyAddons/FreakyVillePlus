114f824dbc0562ae1f343b5d63b5f49ca36ef4e1
WildTooth <champenv@outlook.dk>
2025-02-17 00:43:43 +0100

    Fjernet brug af deprecated annotation

--- core/src/main/java/dk/fvtrademarket/fvplus/core/configuration/DiscordSubConfiguration.java
+++ core/src/main/java/dk/fvtrademarket/fvplus/core/configuration/DiscordSubConfiguration.java
@@ -2,13 +2,12 @@ package dk.fvtrademarket.fvplus.core.configuration;
 
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.configuration.loader.Config;
-import net.labymod.api.configuration.loader.annotation.ParentSwitch;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 import dk.fvtrademarket.fvplus.core.module.general.RPCModule;
 import dk.fvtrademarket.fvplus.core.util.Setting;
 
 public class DiscordSubConfiguration extends Config {
-  @ParentSwitch
+
   @SwitchSetting
   private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
 
